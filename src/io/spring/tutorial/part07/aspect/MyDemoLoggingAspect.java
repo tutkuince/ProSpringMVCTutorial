0e4e5f67acfaf31b5d96d3eb7c3c6b8de785d8c4
tutkuince <tutku.ince@outlook.com>
2018-07-30 17:59:11 +0300

    usage * in a aspect class

--- src/io/spring/tutorial/part07/aspect/MyDemoLoggingAspect.java
+++ src/io/spring/tutorial/part07/aspect/MyDemoLoggingAspect.java
@@ -12,7 +12,8 @@ public class MyDemoLoggingAspect {
 
 	// let's start with an @Before advice
 
-	@Before("execution(public void addAccount())")
+	// @Before("execution(public void addAccount())")
+	@Before("execution(public void add*())")	// any match start with add...
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n======>>> Executing @Before advice on addAccount()");
 	}
