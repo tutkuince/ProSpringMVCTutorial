529dac4af3be467b8cdff60576160d0be913c884
tutkuince <tutku.ince@outlook.com>
2018-07-30 17:07:43 +0300

    dao implementation

---
+++ src/io/spring/tutorial/part07/dao/AccountDAOImpl.java
@@ -0,0 +1,14 @@
+package io.spring.tutorial.part07.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountDAOImpl implements AccountDAO{
+
+	@Override
+	public void addAccount() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
