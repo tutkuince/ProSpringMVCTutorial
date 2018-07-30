f87698f69767e8ee8d7a5753ef362f555492c12f
tutkuince <tutku.ince@outlook.com>
2018-07-30 17:21:05 +0300

    main app

--- src/io/spring/tutorial/part07/main/MainDemoApp.java
+++ src/io/spring/tutorial/part07/main/MainDemoApp.java
@@ -1,7 +1,23 @@
 package io.spring.tutorial.part07.main;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import io.spring.tutorial.part07.config.DemoConfig;
+import io.spring.tutorial.part07.dao.AccountDAO;
+import io.spring.tutorial.part07.dao.AccountDAOImpl;
+
 public class MainDemoApp {
 	public static void main(String[] args) {
-		
+		// read spring config java calss
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+		// get the bean from spring container
+		AccountDAO accountDAO = context.getBean("accountDAOImpl", AccountDAOImpl.class);
+
+		// call the business method
+		accountDAO.addAccount();
+
+		// close the context
+		context.close();
 	}
 }
