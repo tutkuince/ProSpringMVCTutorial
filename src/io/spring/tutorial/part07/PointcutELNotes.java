cd0c44ca1a201a5b70a4f8df1276d9d161ed3622
tutkuince <tutku.ince@outlook.com>
2018-07-30 17:49:57 +0300

    features

--- src/io/spring/tutorial/part07/PointcutELNotes.java
+++ src/io/spring/tutorial/part07/PointcutELNotes.java
@@ -1,9 +1,15 @@
 package io.spring.tutorial.part07;
 
+
 public class PointcutELNotes {
 	/**
 	 * Pointcut Expression Language
 	 * execution(modifiers-pattern? return-type-pattern declaring-type-pattern? method-name-pattern(param-pattern) throws-pattern?)
+	 * 
+	 *  + The pattern is optional if it has '?'
+	 *  + Patterns can use wildcards
+	 *  
+	 *  @Before("execution(public void addAccount())")
 	 * 
 	 * */
 }
