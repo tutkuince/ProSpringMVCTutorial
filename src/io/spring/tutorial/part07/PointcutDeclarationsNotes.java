fe76b718326491be667b1de35418ae00df003fbe
tutkuince <tutku.ince@outlook.com>
2018-08-01 14:45:39 +0300

    notes of PointcutDeclaration

---
+++ src/io/spring/tutorial/part07/PointcutDeclarationsNotes.java
@@ -0,0 +1,12 @@
+package io.spring.tutorial.part07;
+
+public class PointcutDeclarationsNotes {
+	/**
+	 * Create a pointcut declaration once
+	 * Apply it to multiple advices
+	 * 
+	 * @Pointcut("execution(* io.spring.tutorial.part07.dao.*.*(..))")
+	 * private void forDaoPackage(){}
+	 * 
+	 * */
+}
