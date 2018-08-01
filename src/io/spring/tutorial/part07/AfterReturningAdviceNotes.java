a3712e3997377a95c033077ae05644341cf85a6d
tutkuince <tutku.ince@outlook.com>
2018-08-01 18:47:08 +0300

    Notes about @AfterReturning Advice

---
+++ src/io/spring/tutorial/part07/AfterReturningAdviceNotes.java
@@ -0,0 +1,21 @@
+package io.spring.tutorial.part07;
+
+public class AfterReturningAdviceNotes {
+	/**
+	 * @AfterReturning Advice
+	 * This advice will run after the method call (success execution)
+	 * 
+	 * Use Cases
+	 * 
+	 * Most common 
+	 * 	+ logging, security, transactions
+	 * 
+	 * Audit logging
+	 * 	+ who, what, when, where
+	 * 
+	 * Post-processing Data
+	 * 	+ Post process the data before returning to caller
+	 *  + Format the data or enrich the data ( really cool but be careful )
+	 * 
+	 * */
+}
