932c0a94efe35813bca87d8a8754891309fa434d
tutkuince <tutku.ince@outlook.com>
2018-08-01 15:27:01 +0300

    added new fields to AccountDAOImpl

--- src/io/spring/tutorial/part07/dao/AccountDAOImpl.java
+++ src/io/spring/tutorial/part07/dao/AccountDAOImpl.java
@@ -7,6 +7,29 @@ import io.spring.tutorial.part07.model.Account;
 @Component
 public class AccountDAOImpl implements AccountDAO {
 
+	private String name;
+	private String serviceCode;
+
+	public String getName() {
+		System.out.println(getClass() + ": in getName()");
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println(getClass() + ": in setName()");
+		this.name = name;
+	}
+
+	public String getServiceCode() {
+		System.out.println(getClass() + ": in getServiceCode()");
+		return serviceCode;
+	}
+
+	public void setServiceCode(String serviceCode) {
+		System.out.println(getClass() + ": in setServiceCode()");
+		this.serviceCode = serviceCode;
+	}
+
 	@Override
 	public void addAccount(Account account, boolean vipFlag) {
 		System.out.println(getClass() + ": Doing my db work: adding an account");
