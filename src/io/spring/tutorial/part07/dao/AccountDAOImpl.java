87e59bcbe168371c2aa12ab9801fbfb0ed45b46c
tutkuince <tutku.ince@outlook.com>
2018-08-01 14:17:48 +0300

    modified: add param to addAccount method

--- src/io/spring/tutorial/part07/dao/AccountDAOImpl.java
+++ src/io/spring/tutorial/part07/dao/AccountDAOImpl.java
@@ -2,11 +2,13 @@ package io.spring.tutorial.part07.dao;
 
 import org.springframework.stereotype.Component;
 
+import io.spring.tutorial.part07.model.Account;
+
 @Component
 public class AccountDAOImpl implements AccountDAO {
 
 	@Override
-	public void addAccount() {
+	public void addAccount(Account account) {
 		System.out.println(getClass() + ": Doing my db work: adding an account");
 	}
 
