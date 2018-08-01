c543bb251cc4f4d662ca1436dfc28f19729f2151
tutkuince <tutku.ince@outlook.com>
2018-08-01 15:28:34 +0300

    used getter/setter method on AccountDAOImpl

--- src/io/spring/tutorial/part07/main/MainDemoApp.java
+++ src/io/spring/tutorial/part07/main/MainDemoApp.java
@@ -3,26 +3,32 @@ package io.spring.tutorial.part07.main;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import io.spring.tutorial.part07.config.DemoConfig;
-import io.spring.tutorial.part07.dao.AccountDAO;
 import io.spring.tutorial.part07.dao.AccountDAOImpl;
-import io.spring.tutorial.part07.dao.MembershipDAO;
 import io.spring.tutorial.part07.dao.MembershipDAOImpl;
 import io.spring.tutorial.part07.model.Account;
 
 public class MainDemoApp {
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		// read spring config java calss
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
 
 		// get the bean from spring container
-		AccountDAO accountDAO = context.getBean("accountDAOImpl", AccountDAOImpl.class);
+		AccountDAOImpl accountDAO = context.getBean("accountDAOImpl", AccountDAOImpl.class);
 		
 		// get membership bean from spring container
-		MembershipDAO membershipDAO = context.getBean("membershipDAOImpl", MembershipDAOImpl.class);
+		MembershipDAOImpl membershipDAO = context.getBean("membershipDAOImpl", MembershipDAOImpl.class);
 
 		// call the business method
 		accountDAO.addAccount(new Account(), true);
 		accountDAO.doWork();
+		
+		// call the accountdao getter/setter methods
+		accountDAO.setName("foobar");
+		accountDAO.setServiceCode("silver");
+		
+		String name = accountDAO.getName();
+		String code = accountDAO.getServiceCode();
 		
 		// call the membership business method
 		membershipDAO.addMember();
