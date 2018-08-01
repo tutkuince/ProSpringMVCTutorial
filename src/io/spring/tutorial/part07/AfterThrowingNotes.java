bd0b1ab99a8297178975ace9f79232bc73fbce68
tutkuince <tutku.ince@outlook.com>
2018-08-01 20:11:31 +0300

    notes about @AfterThrowing advice

---
+++ src/io/spring/tutorial/part07/AfterThrowingNotes.java
@@ -0,0 +1,14 @@
+package io.spring.tutorial.part07;
+
+public class AfterThrowingNotes {
+	/**
+	 * This advice will run after an exception is thrown
+	 * 
+	 * Use Cases
+	 *  + Log the exception
+	 *  + Perform auditing on the exception
+	 *  + Notify DevOps team via email or SMS
+	 *  + Encapsulate this functionality in AOP aspect for easy reuse
+	 * 
+	 * */
+}
