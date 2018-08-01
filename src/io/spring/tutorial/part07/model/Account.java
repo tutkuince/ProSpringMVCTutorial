6b3dd6ae4158831a64c0ccdb4931148b92540bd8
tutkuince <tutku.ince@outlook.com>
2018-08-01 14:19:17 +0300

    added fields to Account class

--- src/io/spring/tutorial/part07/model/Account.java
+++ src/io/spring/tutorial/part07/model/Account.java
@@ -1,5 +1,25 @@
 package io.spring.tutorial.part07.model;
 
 public class Account {
+	private String name;
+	private String level;
+
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLevel() {
+		return level;
+	}
+
+	public void setLevel(String level) {
+		this.level = level;
+	}
 
 }
