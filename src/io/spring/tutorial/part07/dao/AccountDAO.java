a677da65013e9c4234991d3c9d299eb670a4754e
tutkuince <tutku.ince@outlook.com>
2018-08-01 14:17:40 +0300

    modified: add param to addAccount method

--- src/io/spring/tutorial/part07/dao/AccountDAO.java
+++ src/io/spring/tutorial/part07/dao/AccountDAO.java
@@ -1,5 +1,7 @@
 package io.spring.tutorial.part07.dao;
 
+import io.spring.tutorial.part07.model.Account;
+
 public interface AccountDAO {
-	void addAccount();
+	void addAccount(Account account);
 }
