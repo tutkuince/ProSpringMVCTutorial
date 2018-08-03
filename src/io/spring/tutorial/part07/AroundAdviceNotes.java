eb11f80b622af5b1cf75c007fa43420099ca511f
tutkuince <tutku.ince@outlook.com>
2018-08-03 18:18:07 +0300

    resolving print order notes

--- src/io/spring/tutorial/part07/AroundAdviceNotes.java
+++ src/io/spring/tutorial/part07/AroundAdviceNotes.java
@@ -16,5 +16,13 @@ public class AroundAdviceNotes {
 	 * 	+ This is a handle to the target method
 	 * 	+ Your code can use the proceeding join point to execute target method
 	 * 
+	 * Resolving Print Order Issue
+	 * + Root Cause
+	 * 		- The data is printing to two different output streams
+	 * 		- Spring is printing to the logger output stream
+	 * 		- System.out.prinln is printing to the standard out output stream
+	 * + Solution
+	 * 		- To have everything in order, you should send to same output stream
+	 * 		- We'll change our code to use the logger output stream ... same as Spring
 	 * */
 }
