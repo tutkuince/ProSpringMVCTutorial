bbd8aaf6ffdbf4f4dcbc842f11c66b82c99c6378
tutkuince <tutku.ince@outlook.com>
2018-08-03 18:39:38 +0300

    created AroundHandleExceptionApp for testing

---
+++ src/io/spring/tutorial/part07/main/AroundHandleExceptionDemoApp.java
@@ -0,0 +1,35 @@
+package io.spring.tutorial.part07.main;
+
+import java.util.logging.Logger;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import io.spring.tutorial.part07.config.DemoConfig;
+import io.spring.tutorial.part07.service.TrafficFortuneService;
+
+public class AroundHandleExceptionDemoApp {
+
+	private static Logger logger = Logger.getLogger(AroundHandleExceptionDemoApp.class.getName());
+
+	public static void main(String[] args) {
+		// read spring configuration java class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+		// get the bean from the spring container
+		TrafficFortuneService fortuneService = context.getBean("trafficFortuneService", TrafficFortuneService.class);
+
+		logger.info("\nMain Program: AroundDemoApp");	// System.out.println("\nMain Program: AroundDemoApp");
+
+		logger.info("Calling getFortune()");
+		
+		boolean tripWire = true;
+		
+
+		logger.info("\nMy fortune is: " + fortuneService.getFortune(tripWire));
+
+		logger.info("Finished!");
+
+		// close the context
+		context.close();
+	}
+}
